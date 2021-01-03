b7a355a85a2b117b8763f5833b9edc962ccf7ab9
lucas alexandre <lucasbez14@outlook.com>
2021-01-03 18:19:34 -0300

    php

---
+++ PHP/PHP(curso completo de PHP)/PDO/PDOUsandoTransações.php
@@ -0,0 +1,17 @@
+<?php
+
+$conn = new PDO("mysql:dbname=dbphp7; host=localhost", "root", "root" );
+
+$conn->beginTransaction();
+
+$stmt = $conn->prepare("DELETE FROM tb_usuarios WHERE idusuario = ?");
+
+$id = 2;
+
+$stmt->execute(array($id));
+
+//$conn->rollBack();
+$conn->commit();
+
+echo "Dados deletados com sucesso";
+
