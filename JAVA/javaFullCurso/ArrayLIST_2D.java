c02bf60d1cc03c4393c72f80e5033bbe5da26046
lucas alexandre <lucasbez14@outlook.com>
2021-04-10 12:59:07 -0300

    java

--- JAVA/javaFullCurso/ArrayLIST_2D.java
+++ JAVA/javaFullCurso/ArrayLIST_2D.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
+
 public class ArrayLIST_2D{
     public static void main(String[] args) {
 
-        ArrayList<ArrayList<Strings>> groceryList = new ArrayList();
+        ArrayList<ArrayList<String>> groceryList = new ArrayList();
         
         ArrayList<String> bakeryList = new ArrayList();
         bakeryList.add("pasta");
